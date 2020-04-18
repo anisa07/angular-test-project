5da2909f42b3a5b5a2fece172437178c180497d0
anisa07 <askany07@gmail.com>
2020-04-19 00:00:18 +0300

    add linters, components, classes, interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FakeLogoComponent } from './components/fake-logo/fake-logo.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CourseComponent } from './components/course/course.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FakeLogoComponent,
+    CoursesComponent,
+    CourseComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/project.interfaces.ts
@@ -0,0 +1,13 @@
+export interface UserInterface {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface CourseInterface {
+  id: string;
+  creationDate: Date;
+  duration: number;
+  description: string;
+  title: string;
+}
