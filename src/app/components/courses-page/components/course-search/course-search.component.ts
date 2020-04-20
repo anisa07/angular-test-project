9bba88d4a1e81ff76a6318ed0e23985ccb9dace6
anisa07 <askany07@gmail.com>
2020-04-20 21:14:55 +0300

    remove constructors, hooks

--- src/app/components/courses-page/components/course-search/course-search.component.ts
+++ src/app/components/courses-page/components/course-search/course-search.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-course-search',
   templateUrl: './course-search.component.html',
   styleUrls: ['./course-search.component.scss']
 })
-export class CourseSearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CourseSearchComponent {
 }
